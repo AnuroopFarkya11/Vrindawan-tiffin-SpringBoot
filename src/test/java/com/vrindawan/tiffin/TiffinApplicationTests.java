29561ca330168c4245dd8251ceece1e95f49b4af
AnuroopFarkya11 <anuroopf02@gmail.com>
2024-08-31 02:24:03 +0530

    first commit

---
+++ src/test/java/com/vrindawan/tiffin/TiffinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrindawan.tiffin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiffinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
