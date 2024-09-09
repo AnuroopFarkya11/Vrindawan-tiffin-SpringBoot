84b976ab8b6b962de58426dc0d394a41a7fa9f60
AnuroopFarkya11 <anuroopf02@gmail.com>
2024-09-10 00:03:41 +0530

    Enabled Transactional annotation with transaction manager also, integrated atlas

--- src/main/java/com/vrindawan/tiffin/TiffinApplication.java
+++ src/main/java/com/vrindawan/tiffin/TiffinApplication.java
@@ -2,12 +2,23 @@ package com.vrindawan.tiffin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class TiffinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiffinApplication.class, args);
+	}
+
+	@Bean
+	public TransactionManager transactionManager(MongoDatabaseFactory factory) {
+		return new MongoTransactionManager(factory);
 	}
 
 }
